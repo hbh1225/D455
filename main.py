444a987088a19e8be1eab66bcef486afe61ace0e
hbh1225 <huangbinhua1225@gmail.com>
2024-12-09 14:00:52 +0800

    main.py

--- main.py
+++ main.py
@@ -83,8 +83,6 @@ try:
             Dt = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
             scipy.io.savemat(f'RGBD_D455_{Dt}.mat', mdict={'Depth': Depth, 'Color': Color})
             print(f"Saved RGBD data as RGBD_D455_{Dt}.mat")
-        if key == ord('s'):  # 's' to save point cloud as .ply
-            Dt = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
             colorized = colorizer.process(aligned_frames)
             ply = rs.save_to_ply(f"RGBD_D455_{Dt}.ply")
             ply.set_option(rs.save_to_ply.option_ply_binary, True)
